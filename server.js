bb6ecc92d28abe8957662eb330e2f66a06b6a2a6
rafaelcall100-source <rafaelcall100@gmail.com>
2026-04-10 11:16:53 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -144,8 +144,8 @@ app.get('/send-simple', async (req, res) => {
 // =======================
 // PORTA (RAILWAY)
 // =======================
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log('🚀 Servidor rodando na porta', PORT);
 });
